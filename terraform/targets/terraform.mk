4180397234816c2577e124a8c644004768a86828
Kevin De Notariis <kevin.denotariis@gmail.com>
2023-04-12 13:25:03 +0000

    feature: Adding backend config for terraform init and check of env variables

--- terraform/targets/terraform.mk
+++ terraform/targets/terraform.mk
@@ -1,11 +1,21 @@
+.check-env-variables:
+	@ test $${TERRAFORM_BACKEND_BUCKET_NAME?Please set env variable TERRAFORM_BACKEND_BUCKET_NAME}
+	@ test $${TERRAFORM_BACKEND_KEY?Please set env variable TERRAFORM_BACKEND_KEY}
+	@ test $${TERRAFORM_BACKEND_REGION?Please set env variable TERRAFORM_BACKEND_REGION}
+	@ test $${TERRAFORM_BACKEND_DYNAMODB_TABLE?Please set env variable TERRAFORM_BACKEND_DYNAMODB_TABLE}
+
 # -----------------------------------------------------------------------------------
 # Terraform targets
 # -----------------------------------------------------------------------------------
-terraform/init:
+terraform/init: .check-env-variables
 	@echo "Terraform Init"
 	@cd ${TERRAFORM_FOLDER} && \
 		rm -rf .terraform && \
-		terraform init
+		terraform init \
+			-backend-config="bucket=$(TERRAFORM_BACKEND_BUCKET_NAME)"	\
+			-backend-config="key=$(TERRAFORM_BACKEND_KEY)" \
+			-backend-config="region=$(TERRAFORM_BACKEND_REGION)" \
+			-backend-config="dynamodb_table=$(TERRAFORM_BACKEND_DYNAMODB_TABLE)"
 
 terraform/plan:
 	@echo "Terraform Plan"
