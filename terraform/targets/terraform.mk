ada82818d633fa8bcfa01e2b73529ae3ec856b73
Kevin De Notariis <kevin.denotariis@gmail.com>
2023-04-13 09:19:30 +0000

    feature: Check and set the git credentials to pull private repos

--- terraform/targets/terraform.mk
+++ terraform/targets/terraform.mk
@@ -1,21 +1,31 @@
+REMOTE_SOURCE_CONTROL ?= github.com
+
 .check-env-variables:
-	@ test $${TERRAFORM_BACKEND_BUCKET_NAME?Please set env variable TERRAFORM_BACKEND_BUCKET_NAME}
-	@ test $${TERRAFORM_BACKEND_KEY?Please set env variable TERRAFORM_BACKEND_KEY}
-	@ test $${TERRAFORM_BACKEND_REGION?Please set env variable TERRAFORM_BACKEND_REGION}
-	@ test $${TERRAFORM_BACKEND_DYNAMODB_TABLE?Please set env variable TERRAFORM_BACKEND_DYNAMODB_TABLE}
+	@test $${TERRAFORM_BACKEND_BUCKET_NAME?Please set env variable TERRAFORM_BACKEND_BUCKET_NAME}
+	@test $${TERRAFORM_BACKEND_KEY?Please set env variable TERRAFORM_BACKEND_KEY}
+	@test $${TERRAFORM_BACKEND_REGION?Please set env variable TERRAFORM_BACKEND_REGION}
+	@test $${TERRAFORM_BACKEND_DYNAMODB_TABLE?Please set env variable TERRAFORM_BACKEND_DYNAMODB_TABLE}
+
+.check-and-set-git-creds:
+	@if [ "${GIT_USERNAME}" != "" ] && [ "${GIT_TOKEN}" != "" ]; then\
+		echo "Git credentials found, setting up the configuration...";\
+		git config --global url."https://${GIT_USERNAME}:${GIT_TOKEN}@${REMOTE_SOURCE_CONTROL}".insteadOf https://${REMOTE_SOURCE_CONTROL};\
+	else\
+		echo "No Git credentials found, continuing...";\
+	fi
 
 # -----------------------------------------------------------------------------------
 # Terraform targets
 # -----------------------------------------------------------------------------------
-terraform/init: .check-env-variables
+terraform/init: .check-env-variables .check-and-set-git-creds
 	@echo "Terraform Init"
 	@cd ${TERRAFORM_FOLDER} && \
 		rm -rf .terraform && \
 		terraform init \
-			-backend-config="bucket=$(TERRAFORM_BACKEND_BUCKET_NAME)"	\
-			-backend-config="key=$(TERRAFORM_BACKEND_KEY)" \
-			-backend-config="region=$(TERRAFORM_BACKEND_REGION)" \
-			-backend-config="dynamodb_table=$(TERRAFORM_BACKEND_DYNAMODB_TABLE)"
+			-backend-config="bucket=${TERRAFORM_BACKEND_BUCKET_NAME}"	\
+			-backend-config="key=${TERRAFORM_BACKEND_KEY}" \
+			-backend-config="region=${TERRAFORM_BACKEND_REGION}" \
+			-backend-config="dynamodb_table=${TERRAFORM_BACKEND_DYNAMODB_TABLE}"
 
 terraform/plan:
 	@echo "Terraform Plan"
